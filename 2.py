f7e1c325d7ec0ea5d7bf0ce89c969845769bb4a8
L_A_V_game <vipaleksei007@gmail.com>
2023-12-01 18:12:06 +0300

    job

---
+++ 2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Заданные множества строк
+A = {'c', 'g', 'h', 'k', 'y'}
+B = {'a', 'b', 'k', 'n', 'u'}
+C = {'c', 'd', 'k', 'l', 'y'}
+D = {'a', 'g', 'h', 'u', 'v', 'z'}
+#u = {'a', 'b', 'c', 'd','e', 'f', 'g', 'h',
+#'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+#'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+g = set("abcdefghijklmnopqrstuvwxyz")
+# Операции с использованием union вместо or
+
+an = g.difference(A)
+
+Y = (an.intersection(D)).union(C.difference(B))
+X = A.intersection(B).union(C)
+
+# Вывод результатов
+print(f"Результат Y: {Y}")
+print(f"Результат X: {X}")
